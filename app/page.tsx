738bc4bf06ecf7bc6235d08fe4f2c35555a3f5bc
Pedram <p.vosooghinia69@gmail.com>
2024-08-06 16:40:59 +0330

    feature hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,27 @@
 
 import { navItems } from "@/data";
 
-// import Hero from "@/components/Hero";
-// import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
 import Clients from "@/components/Clients";
-// import Approach from "@/components/Approach";
+import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 // import RecentProjects from "@/components/RecentProjects";
-// import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
     <main className="relative bg-yellow-400 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <Hero />
-        <Grid />
+        <Hero />
+        {/* <Approach /> */}
+        {/* <Grid /> */}
         <FloatingNav navItems={navItems} />
-        <RecentProjects />
-        <Approach /> */}
         <Clients />
         <Experience />
         <Footer />
+        {/* <RecentProjects /> */}
       </div>
     </main>
   );
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -42,10 +42,7 @@
 
 //               <p
 //                 className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-//                 style={{
-//                   color: "#BEC1DD",
-//                   margin: "1vh 0",
-//                 }}
+          
 //               >
 //                 {item.des}
 //               </p>
